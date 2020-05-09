b340df3437436fff920f8446be5b4ed3a972a19d
Nikita Usoltsev <usoltsev-nv@yandex.ru>
2020-05-10 02:49:48 +0300

    edited operator* in where_enum

--- lab_15/include/linq.hpp
+++ lab_15/include/linq.hpp
@@ -1,8 +1,258 @@
-//
-// Created by Nikita Usoltsev on 07.05.2020.
-//
-
 #ifndef LAB_15_LINQ_HPP
 #define LAB_15_LINQ_HPP
+
+#include <utility>
+#include <vector>
+
+namespace linq {
+    namespace impl {
+
+        template<typename T, typename Iter>
+        class range_enumerator;
+        template<typename T>
+        class drop_enumerator;
+        template<typename T, typename U, typename F>
+        class select_enumerator;
+        template<typename T, typename F>
+        class until_enumerator;
+        template<typename T>
+        class take_enumerator;
+        template<typename T, typename F>
+        class where_enumerator;
+
+
+        template<typename T>
+        class enumerator {
+        public:
+            virtual const T& operator*() = 0; // Получает текущий элемент.
+            virtual enumerator<T>& operator++() = 0;  // Переход к следующему элементу
+            virtual explicit operator bool() = 0;  // Возвращает true, если есть текущий элемент
+
+            auto drop(size_t count) {
+                return impl::drop_enumerator<T>(*this, count);
+            }
+
+            auto take(size_t count) {
+                return impl::take_enumerator<T>(*this, count);
+            }
+
+            template<typename U = T, typename F>
+            auto select(F func) {
+                return impl::select_enumerator<U, T, F>(*this, func);
+            }
+
+            template<typename F>
+            auto until(F func) {
+                return impl::until_enumerator<T, F>(*this, func);
+            }
+
+            auto until_eq(const T &x) {
+                return this->until([x](const T &n){ return x == n;});
+            }
+
+            template<typename F>
+            auto where(F func) {
+                return impl::where_enumerator<T, F>(*this, func);
+            }
+
+            auto where_eq(const T &x) {
+                auto f = [x](const T &n){ return x != n;};
+                return this->where(f);
+            }
+
+            std::vector<T> to_vector() {
+                std::vector<T> res;
+                this->copy_to(std::back_inserter(res));
+                return res;
+            }
+
+            template<typename Iter>
+            void copy_to(Iter it) {
+                while (static_cast<bool>(*this)) {
+                    if(&*(*this) == null_element_)
+                        break;
+                    *it = *(*this);
+                    ++it;
+                    ++(*this);
+                }
+            }
+
+        protected:
+            T *null_element_ = nullptr;
+        };
+
+        template<typename T, typename Iter>
+        class range_enumerator : public enumerator<T> {
+        public:
+            range_enumerator(Iter begin, Iter end) : begin_(begin), end_(end) {}
+
+            virtual const T& operator*() override {
+                return *begin_;
+            }
+
+            virtual enumerator<T>& operator++() override {
+                ++begin_;
+                return *this;
+            }
+
+            explicit operator bool() override {
+                return begin_ != end_;
+            }
+
+        private:
+            Iter begin_, end_;
+        };
+
+        template<typename T>
+        class drop_enumerator : public enumerator<T> {
+        public:
+            drop_enumerator(enumerator<T> &parent, size_t count) : parent_(parent),
+                                                                   count_(count),
+                                                                   curr_count_(0)
+                                                                   {}
+
+            virtual const T& operator*() override {
+                while(curr_count_ < count_ && static_cast<bool>(*this))
+                    ++(*this);
+                if(!static_cast<bool>(*this)) return *(enumerator<T>::null_element_);
+                return *parent_;
+            }
+
+            virtual enumerator<T>& operator++() override {
+                ++parent_;
+                ++curr_count_;
+                return *this;
+            }
+
+            explicit operator bool() override {
+                return static_cast<bool>(parent_);
+            }
+        private:
+            enumerator<T> &parent_;
+            size_t count_;
+            size_t curr_count_;
+        };
+
+        template<typename T, typename U, typename F>
+        class select_enumerator : public enumerator<T> {
+        public:
+            select_enumerator(enumerator<U> &parent, F func) : parent_(parent),
+                                                               func_(func)
+                                                               {}
+
+            virtual const T& operator*() override {
+                element_ = func_(*parent_);
+                return element_;
+            }
+
+            virtual enumerator<T>& operator++() override {
+                ++parent_;
+                return *this;
+            }
+
+            virtual explicit operator bool() override {
+                return static_cast<bool>(parent_);
+            }
+
+        private:
+            enumerator<U> &parent_;
+            F func_;
+            T element_;
+        };
+
+        template<typename T, typename F>
+        class until_enumerator : public enumerator<T> {
+        public:
+            until_enumerator(enumerator<T> &parent, F &predicate) : parent_(parent),
+                                                                    predicate_(predicate)
+                                                                    {}
+
+            virtual const T& operator*() override {
+                return *parent_;
+            }
+
+            virtual enumerator<T>& operator++() override {
+                ++parent_;
+                return *this;
+            }
+
+            virtual explicit operator bool() override {
+                return static_cast<bool>(parent_) && !predicate_(*parent_);
+            }
+
+        private:
+            enumerator<T> &parent_;
+            F predicate_;
+        };
+
+        template<typename T>
+        class take_enumerator : public enumerator<T> {
+        public:
+            take_enumerator(enumerator<T> &parent, size_t count) : parent_(parent),
+                                                                   count_(count)
+                                                                   {}
+
+            virtual const T& operator*() override {
+                return *parent_;
+            }
+
+            virtual enumerator<T>& operator++() override {
+                if (count_) {
+                    --count_;
+                    ++parent_;
+                }
+                return *this;
+            }
+
+            explicit operator bool() override {
+                return static_cast<bool>(parent_) && count_;
+            }
+
+        private:
+            enumerator<T> &parent_;
+            size_t count_;
+        };
+
+        template<typename T, typename F>
+        class where_enumerator : public enumerator<T> {
+        public:
+            where_enumerator(enumerator<T> &parent, F func) : parent_(parent),
+                                                              func_(func),
+                                                              is_curr_(static_cast<bool>(parent) && func(*parent))
+                                                              {}
+            virtual const T& operator*() override {;
+                return *parent_;
+            }
+
+            virtual enumerator<T>& operator++() override {
+                iter_correct();
+                is_curr_ = static_cast<bool>(++parent_) && func_(*(parent_));
+                return *this;
+            }
+
+            explicit operator bool() override {
+                iter_correct();
+                return static_cast<bool>(parent_);
+            }
+        private:
+            enumerator<T> &parent_;
+            F func_;
+            bool is_curr_;
+
+            void iter_correct() {
+                while(!is_curr_ && static_cast<bool>(parent_))
+                    is_curr_ = static_cast<bool>(++parent_) && func_(*(parent_));
+            }
+
+        };
+
+    } // namespace impl
+
+    template<typename Iter>
+    auto from(Iter begin, Iter end) {
+        return impl::range_enumerator<typename std::iterator_traits<Iter>::value_type , Iter>(begin, end);
+    }
+
+} // namespace linq
 
 #endif //LAB_15_LINQ_HPP
--- lab_15/src/minimal_smoke_test.cpp
+++ lab_15/src/minimal_smoke_test.cpp
@@ -1,4 +1,205 @@
-//
-// Created by Nikita Usoltsev on 07.05.2020.
-//
+#include "linq.hpp"
+#include <assert.h>
+#include <vector>
+#include <sstream>
+#include <iterator>
+#include <math.h>
+#include <iostream>
 
+using linq::from;
+
+void from_to_vector() {
+    std::vector<int> xs = { 1, 2, 3 };
+    std::vector<int> res = from(xs.begin(), xs.end()).to_vector();
+    assert(res == xs);
+}
+
+void from_select() {
+    const int xs[] = { 1, 2, 3 };
+    std::vector<int> res = from(xs, xs + 3).select([](int x) { return x + 5; }).to_vector();
+    std::vector<int> expected = { 6, 7, 8 };
+    assert(res == expected);
+}
+
+void from_drop_select() {
+    const int xs[] = { 1, 2, 3 };
+    std::vector<int> res = from(xs, xs + 3).drop(1).select([](int x) { return x + 5; }).to_vector();
+    std::vector<int> expected = { 7, 8 };
+    assert(res == expected);
+}
+
+/*
+ * MY TESTS
+ */
+
+void from_until() {
+    std::vector<double>v{1.1, -9.0, 100, 37.7};
+    std::vector<double> res = from(v.begin(), v.end())
+            .until([](double x){ return x >= 100; })
+            .to_vector();
+    std::vector<double> expected = {1.1, -9.0};
+    assert(res == expected);
+}
+
+void from_until_1() {
+    std::vector<double>v{1.1, -9.0, 100, 37.7};
+    std::vector<double> res = from(v.begin(), v.end())
+            .until([](double n){ return n == -9.0; })
+            .to_vector();
+    std::vector<double> expected = {1.1};
+    assert(res == expected);
+}
+
+void from_until_eq() {
+    std::vector<int>v{11, -90, 100, 377};
+    std::vector<int> res = from(v.begin(), v.end())
+            .until_eq(-90)
+            .to_vector();
+    std::vector<int> expected = {11};
+    assert(res == expected);
+}
+
+void from_take() {
+    std::vector<char>v{'1', 'a', 'o', '8'};
+    std::vector<char> res = from(v.begin(), v.end())
+            .take(3)
+            .to_vector();
+    std::vector<char> expected = {'1', 'a', 'o'};
+    assert(res == expected);
+}
+
+void from_drop() {
+    std::stringstream iss("-101 94 116 100 0");
+    std::stringstream oss;
+    std::istream_iterator<int> in(iss), eof;
+    std::ostream_iterator<double> out(oss, "\n");
+
+    from(in, eof)
+            .drop(1000)
+            .copy_to(out);
+    assert(oss.str() == "");
+}
+
+void from_until_3() {
+    std::stringstream iss("1.0 94.0 -16.0 110.0 0.0");
+    std::stringstream oss;
+    std::istream_iterator<double> in(iss), eof;
+    std::ostream_iterator<int> out(oss, " ");
+
+    from(in, eof)
+            .drop(2)
+            .until([](double x){ return x > 10000.0; })
+            .copy_to(out);
+    assert(oss.str() == "-16 110 0 ");
+}
+
+void from_where_1() {
+    std::vector<int>v{1, -1110, 1, -312415, -7, 1, -89, -30};
+    std::vector<int> res = from(v.begin(), v.end())
+            .where([](int x){ return x < 0; })
+            .where([](int x){ return x < -10; })
+            .where([](int x){ return x < -87; })
+            .where([](int x){ return x > -1111; })
+            .to_vector();
+    std::vector<int> expected = {-1110, -89};
+    assert(res == expected);
+}
+
+void from_where_2() {
+    std::vector<int>v{};
+    std::vector<int> res = from(v.begin(), v.end())
+            .where([](int x){ return x < 1726730; })
+            .where([](int x){ return x < -178120; })
+            .to_vector();
+    std::vector<int> expected{};
+    assert(res == expected);
+}
+
+/*
+ * TESTS - EXAMPLES
+ */
+
+void example1() {
+    int xs[] = { 1, 2, 3, 4, 5 };
+
+    std::vector<int> res =
+            from(xs, xs + 5)  // Взять элементы xs
+                    .select([](int x) { return x * x; })  // Возвести в квадрат
+                    .where_eq(25)    // Оставить только значения != 25
+                    .where([](int x) { return x > 3; })   // Оставить только значения > 3
+                    .drop(2)          // Убрать два элемента из начала
+                    .to_vector();     // Преобразовать результат в вектор
+
+    std::vector<int> expected = { 16 };
+    assert(res == expected);
+}
+
+void example2() {
+    std::stringstream ss("1 2 3 -1 4");
+    std::istream_iterator<int> in(ss), eof;
+    std::vector<int> res =
+            from(in, eof)  // Взять числа из входного потока
+                    .take(4)       // Не более четырёх чисел
+                    .until_eq(-1)  // Перестать читать после прочтения -1
+                    .to_vector();  // Получить список считанных чисел
+
+    std::vector<int> expected = { 1, 2, 3 };
+    assert(expected == res);
+
+    int remaining;
+    assert(ss >> remaining);
+    assert(remaining == 4);
+}
+
+void example3() {
+    int xs[] = { 1, 2, 3, 4, 5 };
+
+    std::vector<double> res =
+            from(xs, xs + 5)  // Взять элементы xs
+                    .select<double>([](int x) { return sqrt(x); })  // Извлечь корень
+                    .to_vector();     // Преобразовать результат в вектор
+
+    assert(res.size() == 5);
+    for (std::size_t i = 0; i < res.size(); i++) {
+        assert(fabs(res[i] - sqrt(xs[i])) < 1e-9);
+    }
+}
+
+void example4() {
+    std::stringstream iss("4 16");
+    std::stringstream oss;
+    std::istream_iterator<int> in(iss), eof;
+    std::ostream_iterator<double> out(oss, "\n");
+
+    from(in, eof)    // Взять числа из входного потока
+            .select([](int x) { return static_cast<int>(sqrt(x) + 1e-6); })  // Извлечь из каждого корень
+            .copy_to(out);  // Вывести на экран
+
+    assert(oss.str() == "2\n4\n");
+}
+
+int main() {
+    from_to_vector();
+    from_select();
+    from_drop_select();
+
+    //MY_TESTS
+    from_until();
+    from_until_1();
+    from_until_eq();
+    from_take();
+    from_drop();
+    from_until_3();
+    from_where_1();
+    from_where_2();
+
+    //EXAMPLE
+    example1();
+    example2();
+    example3();
+    example4();
+
+    std::cout << "OK\n";
+
+    return 0;
+}
