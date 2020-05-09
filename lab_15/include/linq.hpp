f5299d5bfdeaa2fa85cf136c7b178ffe908af238
Nikita Usoltsev <usoltsev-nv@yandex.ru>
2020-05-09 13:03:48 +0300

    lab_15 - linq

---
+++ lab_15/include/linq.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Nikita Usoltsev on 07.05.2020.
+//
+
+#ifndef LAB_15_LINQ_HPP
+#define LAB_15_LINQ_HPP
+
+#endif //LAB_15_LINQ_HPP
---
+++ lab_15/src/minimal_smoke_test.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Nikita Usoltsev on 07.05.2020.
+//
+
